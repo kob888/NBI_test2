54f2981d5f80d825bf650b4ffc6237688d15c19d
Vladislav Grachev <vladgrachev88@gmail.com>
2019-07-12 10:57:16 +0300

    first commit

--- Test_2_NBI/Program.cs
+++ Test_2_NBI/Program.cs
@@ -10,6 +10,54 @@ namespace Test_2_NBI
     {
         static void Main(string[] args)
         {
+            List<Interval> dates = new List<Interval>()
+            {
+                new Interval{ Start = new DateTime(2017, 9, 1), End = new DateTime(2020, 12, 10)},
+                new Interval{ Start = new DateTime(2017, 8, 3), End = new DateTime(2019, 1, 9)},
+                new Interval{ Start = new DateTime(2022, 12, 12), End = new DateTime(2023, 5, 10)},
+                new Interval{ Start = new DateTime(2000, 1, 4), End = new DateTime(2001, 7, 5)},
+                new Interval{ Start = new DateTime(2001, 6, 1), End = new DateTime(2001, 8, 5)}
+            };
+
+
+            List<Interval> result = new List<Interval>();
+
+            DateTime _start;
+            DateTime _end;
+            while (dates.Count > 0)
+            {
+                var minDate = dates.Min(t => t.Start);
+                var min = dates.Where(d => d.Start == minDate).First();
+                _start = min.Start;
+                _end = min.End;
+                dates.Remove(min);
+                while (dates.Count > 0)
+                {
+                    if (dates.Any(p => p.Start <= min.End))
+                    {
+                        var x = dates.Where(p => p.Start <= min.End).FirstOrDefault();
+                        _end = x.End;
+                        dates.Remove(x);
+                        continue;
+                    }
+                    break;
+                }
+
+                Interval model = new Interval()
+                {
+                    Start = _start,
+                    End = _end
+                };
+                result.Add(model);
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine("{0} {1}", item.Start.ToString("dd/MM/yyyy"), item.End.ToString("dd/MM/yyyy"));
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
